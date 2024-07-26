ddd7eb0c59b423c19b2897ed2b9355ac23ec30d2
Maytastico <may_grune@hotmail.com>
2024-07-26 11:46:26 +0200

    #46-swagger-file

--- api-backend/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type {Config} from 'jest';
-
-const config: Config = {
-    verbose: true,
-    moduleDirectories: ['node_modules', 'source'],
-    testMatch: ['<rootDir>/source/**/*.test.ts']
-  };
-
-export default config;
--- api-backend/source/controllers/user.controller.ts
+++ api-backend/source/controllers/user.controller.ts
@@ -3,9 +3,14 @@ import { generateAccessToken, registerUserToken } from "../auth/auth";
 
 export const login = async (req: Request, res: Response, next: NextFunction) =>{
     const username: string = req.body.username
-    const token = generateAccessToken(username)
-    registerUserToken(username, token)
-    
-    return res.send(token).status(200);
+    if(username !== undefined){
+        if(username.length > 1){
+            const token = generateAccessToken(username)
+            registerUserToken(username, token)
+            
+            return res.send(token).status(200);
+        }
+    }
+    return res.sendStatus(400);
 }
 
