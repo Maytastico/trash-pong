212aca1266944e000078724e3845f3078c518781
CaroE12 <caropap20@gmail.com>
2024-07-28 18:49:02 +0200

    #45_Testlobbys_anzeigen_Raum_erstellen_updaten_und_loeschen_hinzugefuegt

--- api-backend/source/routes/api.ts
+++ api-backend/source/routes/api.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-import {dbRoom} from '../controllers/api.controller';
-import { dbAllRooms } from '../controllers/api.controller';
+import {dbRoom, dbAllRooms} from '../controllers/api.controller';
+import {dbCreateRoom} from '../controllers/api.controller'; 
+import { dbUpdateRoom } from '../controllers/api.controller';
+import { dbDeleteRoom } from '../controllers/api.controller';  
+
 
 const router = express.Router();
 
 router.get("/room/:id", dbRoom);
 router.get("/room", dbAllRooms);
+router.post("/room", dbCreateRoom);
+router.put("/room/:id",dbUpdateRoom);
+router.delete("/room/:id", dbDeleteRoom);
 
 export default router;
