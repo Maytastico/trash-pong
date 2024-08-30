f31ee5ceab514e74d4e99907ae6673bfe98441de
Maytastico <may_grune@hotmail.com>
2024-08-30 15:47:40 +0200

    room auth check

--- api-backend/source/gameclient/socket.ts
+++ api-backend/source/gameclient/socket.ts
@@ -1,29 +1,84 @@
 import { Server as SocketIOServer } from 'socket.io';
 import http from 'http';
+import { SECRET_KEY} from '../auth/token';
+import jwt, {JwtPayload} from 'jsonwebtoken';
 import { decodeAccessToken } from '../auth/auth';
+import { User } from '../types/User';
+import { getRoom, updateRoom } from '../database/room';
+import { Raum } from '../types/Room';
 
 export let io: SocketIOServer;
 
+export interface CustomRequest extends Request {
+    token: string | JwtPayload;
+}
+
 export function initializeWebSocketServer(server: http.Server): void {
     io = new SocketIOServer(server);
+
+    // Middleware zur Validierung der Verbindung
+    io.use((socket, next) => {
+
+        try {
+            const token = socket.request.headers['authorization'];
+         
+            if (!token) {
+              throw new Error("No Bearer Token set in header");
+            }
+         
+            const decoded = jwt.verify(token, SECRET_KEY);
+            console.log(decoded);
+         
+            next();
+          } catch (err) {
+            next(new Error("Invalid Authentication")); // Verbindung ablehnen
+          }
+        
+
+    });
 
     io.on('connection', (ws) => {
         console.log(`User connected: ${ws.id}`);
         console.log(`Current rooms: ${Array.from(ws.rooms).join(', ')}`);
     
         // Handle 'joinRoom' Event
-        ws.on('joinRoom', (data: { roomId: string; playerId: string }) => {
-            const { roomId, playerId } = data;
-            
+        ws.on('joinRoom', async (data: { roomId: number;}) => {
+            const { roomId} = data;
 
+            const token = ws.request.headers['authorization'];
+                // Überprüfe, ob der Token existiert
+            if (!token) {
+                console.error('Authorization header is missing.');
+                ws.emit('error', 'Authorization header is missing');
+                return;
+            }
+            // decodeAccessToken sollte einen Fehler werfen, wenn der Token ungültig ist
+            let player: User;
+            try {
+                player = decodeAccessToken(token);
+            } catch (err) {
+                console.error('Invalid token');
+                ws.emit('error', 'Invalid token');
+                return;
+            }
+            
+            let room = await getRoom(roomId);
+
+            if(!room){
+                throw new Error("Room does not exist");
+            }
+
+            if (room.user_id1 !== player.user_id){
+                await updateRoom(room.room_id, room.titel, room.passwort, room.öffentlich, room.user_id1, player.user_id);
+            }
 
             // Join the room
-            ws.join(roomId);
-            console.log(`User ${playerId} joined room ${roomId}`);
+            ws.join(roomId.toString());
+            console.log(`User ${player.name} joined room ${roomId}`);
             console.log(`Updated rooms: ${Array.from(ws.rooms).join(', ')}`);
             
             // Notify other users in the room
-            ws.to(roomId).emit('notification', `${playerId} has joined the room`);
+            ws.to(roomId.toString()).emit('notification', `${player.name} has joined the room`);
         });
     
         // Handle 'sendMessage' Event
