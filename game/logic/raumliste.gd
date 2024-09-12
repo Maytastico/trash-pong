58b312350a57f94b168e4ec77c8db3f962f1ad4b
Aron Seidl <aron.seidl@zeiss.com>
2024-09-12 15:33:28 +0200

    cant join rooms wehen full now

--- game/logic/raumliste.gd
+++ game/logic/raumliste.gd
@@ -27,7 +27,8 @@ func _on_join_room_pressed():
 	else:
 		Global.activeRoomID = Global.rooms[selected_items[0]].raum_id
 		Global.activeRoom = Global.rooms[selected_items[0]]
-		if Global.activeRoom.player2 != "":
+		
+		if Global.activeRoom.player2 == "":
 			if(!checkForPassword(Global.activeRoom)):
 				var imraum = load("res://im_raum.tscn").instantiate()
 				get_tree().get_root().add_child(imraum)
